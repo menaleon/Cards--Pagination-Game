06ad3c128be54dad7f86c7e3a42d30e0d9448764
Jimena León Huertas <85376454+menaleon@users.noreply.github.com>
2022-04-15 00:09:22 -0600

    Decompose card indexes - Server side
    Added a function to interpret the indexes sent by the client. The function consists in taking the numbers of a string (which is the message received)

--- QT-Server/server.cpp
+++ QT-Server/server.cpp
@@ -10,8 +10,9 @@ server::server(QWidget *parent) :
     ui(new Ui::server)
 {
     ui->setupUi(this);
-
+    this->setStyleSheet("background-color: lightblue;");
     ui->plainTextEdit->setReadOnly(true);
+
     _server = new QTcpServer(this);
     _server->listen(QHostAddress::Any, 4050);
     _socket = new QTcpSocket(this);
@@ -25,13 +26,52 @@ void server::conexion_nueva(){
 
 void server::leer_socket(){
 
+
+    mensajes_recibidos++;
+
     QByteArray buffer;
     buffer.resize(_socket->bytesAvailable());
     _socket->read(buffer.data(), buffer.size());
 
-    QString message = QString(buffer);
+    QString message = QString(buffer).toLatin1().data();
 
-    ui->plainTextEdit->appendPlainText(message);
+    handle_mensaje(message, mensajes_recibidos);
+
+    ui->plainTextEdit->appendPlainText(message);  
+}
+
+
+void server::descomponer_indices(QString mensaje, int whichCard){
+
+    int fila = mensaje[0].digitValue();
+    int columna = mensaje[1].digitValue();
+
+    if(whichCard == 1){ // Primera carta
+        fila1 = fila;
+        columna1 = columna;
+
+    }else{ // Segunda carta
+        fila2 = fila;
+        columna2 = columna;
+    }
+
+}
+
+void server::handle_mensaje(QString mensaje, int mensajesRecibidos){
+
+    llego_segundaCarta = mensajesRecibidos % 2 == 0 && mensajesRecibidos > 2;
+
+    if(mensajesRecibidos == 1){         // mensaje recibido = nombre player 1
+        ui->nameP1->setText(mensaje);
+
+    }else if(mensajesRecibidos == 2){   // mensaje recibido = nombre player 2
+        ui->nameP2->setText(mensaje);
+
+    }else if(llego_segundaCarta){       // mensaje recibido = segunda carta
+        descomponer_indices(mensaje, 2);
+    }else{                              // mensaje recibido = primera carta
+        descomponer_indices(mensaje, 1);
+    }
 }
 
 server::~server()
@@ -41,10 +81,14 @@ server::~server()
 
 void server::on_send_clicked()
 {
-    const char* message = ui->lineEdit->text().toLatin1().data();
 
-    _socket->write(message, ui->lineEdit->text().size());
+    enviar_al_cliente(ui->lineEdit->text());
 
     ui->plainTextEdit->appendPlainText(ui->lineEdit->text());
     ui->lineEdit->clear();
+}
+
+void server::enviar_al_cliente(QString message){
+    _socket->write(message.toLatin1().data(), message.size());
+
 }
--- QT-Server/server.h
+++ QT-Server/server.h
@@ -20,13 +20,25 @@ public:
 private slots:
     void conexion_nueva();
     void leer_socket();
-
+    void handle_mensaje(QString mensaje, int mensajesRecibidos);
+    void descomponer_indices(QString mensaje, int whichCard);
     void on_send_clicked();
+    void enviar_al_cliente(QString message);
 
 private:
     Ui::server *ui;
     QTcpServer* _server;
     QTcpSocket* _socket;
+
+    int mensajes_recibidos = 0;
+    int player1 = 1;
+    bool llego_segundaCarta;
+
+    int columna1;
+    int fila1;
+
+    int columna2;
+    int fila2;
 };
 
 #endif // SERVER_H
